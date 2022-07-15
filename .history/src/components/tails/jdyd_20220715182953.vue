0d259bc9741b26154dbc11d84f7f48149545c0b1
CuriosityWang <50990182+CuriosityWang@users.noreply.github.com>
2022-07-15 18:56:17 +0800

    jdyd.vue edited

---
+++ .history/src/components/tails/jdyd_20220715182953.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="g-rep">
+    <div class="introduce">
+      <h3>酒店预定</h3>
+      <span>第十四届全国几何设计与计算学术会议</span>
+    </div>
+    <div class="text">
+      <p><strong>会议酒店：汇泉王朝大饭店</strong></p> 
+      <p><strong>地址：中国山东省青岛市市南区南海路9号</strong></p>     <br>
+      <p class="text_indent">汇泉王朝大饭店是一家五星级饭店，坐落于风景秀丽的汇泉湾畔，依山傍海，景色宜人，红瓦绿树、碧海蓝天。数步之间即可与青岛最负盛名的第一海水浴场拥抱，是最具岛城特色的海边旅游渡假饭店之一。汇泉王朝大饭店以一流的设备、一流的管理、一流的服务，为您提供舒适的享受和温馨的氛围。</p> <br>
+      <p style="text-align: center;"><img src="../../assets/wx/jdyd/huiquan.png"></p>
+      <p style="text-align: center;"><img src="../../assets/wx/jdyd/position.png" style="width: 83%;"></p>
+
+      <h4 class="introduce">酒店分布</h4>
+      <p style="text-align: center;"><img src="../../assets/wx/jdyd/jd.png"></p><br>
+      
+      <p> <b>酒店一：青岛汇泉王朝大饭店 <span style="color: red;">(主会场酒店）</span></b> </p> 
+      <ul>
+        <li>地址：青岛市市南区南海路9号</li>
+        <li>价格：标准间700元/晚、大床房700元/晚（协议价格），含早</li>
+        <li>客房预订：18605326331</li>
+      </ul>
+  
+      <p> <b>酒店二：青岛武胜关度假酒店 <span style="color: blue;">(距离主会场150米）</span></b> </p> 
+      <ul>
+        <li>地址：青岛市市南区武昌路3号</li>
+        <li>价格：标准间550元/晚、大床房650元/晚（协议价格），含早</li>
+        <li>客房预订：176602x11888</li>
+      </ul>
+
+      
+      <p> <b>酒店一：青岛汇泉王朝大饭店 <span style="color: red;">(主会场酒店）</span></b> </p> 
+      <ul>
+        <li>地址：青岛市市南区南海路9号</li>
+        <li>价格：标准间700元/晚、大床房700元/晚（协议价格），含早</li>
+        <li>客房预订：18605326331</li>
+      </ul>
+      
+      
+     
+      
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.text {
+  font-size: 20px;
+  line-height: 1.35;
+}
+.text_indent {
+  text-indent: 2em;
+  /* line-height: 30px; */
+}
+.introduce {
+  width: 300px;
+  text-align: center;
+  margin: 40px auto;
+  font-size: 30px;
+}
+.introduce > span {
+  display: inline-block;
+  margin-top: 10px;
+  color: #898989;
+  font-size: 15px;
+}
+</style>
