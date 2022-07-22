2620a52f99c7aceee44c13d23a6be952d033f99c
Cluir <804019821@qq.com>
2022-07-22 22:26:55 +0800

    table center

--- src/components/heads/chzc.vue
+++ src/components/heads/chzc.vue
@@ -6,7 +6,7 @@
     </div>
     <div style="font-size:20px;line-height: 1.35;">
     <p style="font-weight: bold;line-height: 2;">参会收费标准：</p>  
-    <table class="MsoNormalTable" align="center" border="1" cellspacing="0" style="height:150px;width:700px;border:none;">
+    <table class="MsoNormalTable" align="center" border="1" cellspacing="0" style="height:150px;width:700px;border:none;margin:auto">
 	<tbody>
 		<tr>
 			<td width="184" valign="center" rowspan="2" style="border:1.0000pt outset #337FE5;background:#548DD4;">
@@ -107,27 +107,31 @@
 <br/>
   <div  style="font-size:20px;line-height: 1.35;">
       <span  style="font-weight:bold;line-height: 2;">会议注册说明：</span> <br/>
-      <p>1、早鸟注册时间：7月14日00:00-8月13日00:00 <br/>&nbsp;&nbsp;&nbsp;&nbsp;正常注册时间：8月13日00:00-8月20日00:00 <br/>
-      &nbsp;&nbsp;&nbsp;  现场注册时间：8月20日00:00-8月22日00:00</p>
-      <p>2、8月13日前于线上注册的参会嘉宾请于8月20日报到当天领取注册费发票<br/>
+      <p style="text-align:left;">1、早鸟注册时间：7月14日00:00-8月13日00:00 <br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;正常注册时间：8月13日00:00-8月20日00:00 <br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;现场注册时间：8月20日00:00-8月22日00:00</p>
+      <p style="text-align:left;">2、8月13日前于线上注册的参会嘉宾请于8月20日报到当天领取注册费发票<br/>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;8月13日及之后的日期注册的参会嘉宾在两周后可查收电子发票</p>
-      <p>3、 请根据注册情况选择合适的注册类型，每篇录用论文<span style="font-weight:bold">至少1位作者必须以非学生</span>的类型进行注册。同一作者如有多篇论文录用，需要分别独立注册。<br/></p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;会员指“中国工业与应用数学学会”会员，加入中国工业与应用数学学会请参见：<a href="https://www.csiam.org.cn/member/join.php" target="_blank">https://www.csiam.org.cn/member/join.php</a>。对于非学会人员，建议先申请加入学会，获得会员号后即可享受优惠价格注册，申请入会时请选择“<span style="font-weight:bold">几何设计与计算专业委员会</span>”。由于会员系统与会议系统数据不是实时更新，如需享受会员优惠，请提前注册会员并缴纳会费。如新注册会员遇会议系统不能识别的情况，请等待几天再尝试注册会议。</p>
+      <p  style="text-align:justify;">3、请根据注册情况选择合适的注册类型，每篇录用论文<span style="font-weight:bold">至少1位作者必须以非学生</span>的类型进行注册。同一作者如有多篇论文录用，需要分别独立注册。
+      会员指“中国工业与应用数学学会”会员，加入中国工业与应用数学学会请参见：<a href="https://www.csiam.org.cn/member/join.php" target="_blank">https://www.csiam.org.cn/member/join.php</a>。</p>
+	<p style="text-align:justify;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;对于非学会人员，建议先申请加入学会，获得会员号后即可享受优惠价格注册，申请入会时请选择“<span style="font-weight:bold">几何设计与计算专业委员会</span>”。由于会员系统与会议系统数据不是实时更新，如需享受会员优惠，请提前注册会员并缴纳会费。如新注册会员遇会议系统不能识别的情况，请等待几天再尝试注册会议。</p>
       <p>4、注册链接：<a href="https://csiam.org.cn/submission/" target="_blank">https://csiam.org.cn/submission/</a></p>
-      <p style="margin-left: 2em;">
+      <p style="margin-left:30px;text-align:justify;">
         ① 请登录会议注册系统在线注册个人信息，所注册的个人信息将用于铭牌打印。<br/>
 ② 请尽量根据提示使用在线支付转账，不支持支付宝对公转账，便于查询到账金额及开发票。<br/>
 ③  如需集体汇款，请注明“<span style="font-weight:bold">GDC青岛+注册编号+所汇款项所有人名</span>”，集体汇款渠道：<br/>
-账户名：中国工业与应用数学学会<br/>
-账号：01090334600120105034670<br/>
-开户行：北京银行清华园支行<br/>
+</p>
+<p style="margin-left:60px;">账户名：中国工业与应用数学学会<br/>
+账&nbsp;&nbsp;&nbsp;号：01090334600120105034670<br/>
+开户行：北京银行清华园支行<br/></p>
+<p style="margin-left:30px;">
 ④ 如需使用公务卡，建议先将公务卡绑定支付宝，直接进行在线支付；或报到现场刷卡。<br/>
 ⑤ 如需现场缴费，届时可使用微信、支付宝或刷卡；因疫情防控，会议不收取现金。<br/>
       </p>
       <p>
         5、退款政策：已交费代表因故取消参会，退款会产生手续费，将从已交注册费中扣除100元。
       </p>
-      <p style="margin-left: 2em;">① 所有退款申请均需通过邮件发送至gdc@csiam.org.cn邮箱，正文需注明姓名、交费方式、缴费金额、缴费时间，电话或口头提出均无效。<br/>
+      <p style="margin-left:30px;">① 所有退款申请均需通过邮件发送至gdc@csiam.org.cn邮箱，正文需注明姓名、交费方式、缴费金额、缴费时间，电话或口头提出均无效。<br/>
 ② 为便于管理，所有退款申请将于会后统一按支付路径退回。<br/>
 ③ 各种原因需要退费的代表需在会议结束后的一个月之内（9月23日前）提出申请，过时无效；<br/>
 ④ 收到申请后，如人数较多，将于两个月之内完成退费手续（最终到账时间取决于支付方式和银行处理时效）<br/>
