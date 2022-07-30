38671445a34753ea0b38992eac4c00ab457328a9
CuriosityWang <50990182+CuriosityWang@users.noreply.github.com>
2022-07-30 15:41:23 +0800

    update part of ztlt.vue

--- src/components/heads/ztlt.vue
+++ src/components/heads/ztlt.vue
@@ -4,7 +4,188 @@
       <h3>主题论坛</h3>
       <!-- <span>第十四届全国几何设计与计算学术会议</span> -->
     </div>
-    <h2>待更新...</h2>
+    <div class="content-main">
+      <h1>论坛列表</h1>
+      <ul>
+        <li><b><a href="#panel_1">技术论坛1：CAD曲面求交论坛</a></b></li>
+        <li><b><a href="#panel_2">技术论坛2：图形的“引擎”-产业技术特别论坛</a></b></li>
+        <li><b><a href="#panel_3">技术论坛3：智慧农牧论坛</a></b></li>
+        <li><b><a href="#panel_4">技术论坛4：图形学教学研讨论坛</a></b></li>
+        <li><b><a href="#panel_5">技术论坛5：图形学杰出青年论坛</a></b></li>
+        <li><b><a href="#panel_6">技术论坛6：战略前沿技术论坛</a></b></li>
+        <li><b><a href="#panel_7">SIGGRAPH之夜暨优秀博士生论坛</a></b></li>
+      </ul>
+      <div id="panel_1"></div>
+      <h1>国产CAD几何引擎</h1>
+      <p><b>论坛简介</b></p>
+      <p style="text-indent: 2em">
+        几何引擎是CAD软件的核心模块。我国的CAD软件在系统稳定性和生态完整性上与国际先进水平尚有一定差距，特别是在自主产权的CAD几何引擎上呈现卡脖子和研究断档态势，链条式地影响到航空、航天、国防等核心制造领域。近年来，国家重启了对CAD几何引擎和CAD软件研发的战略部署，本论坛将邀请到我国CAD学术和工业界的资深人士，共谈国产CAD几何引擎的过去、现状和未来。
+      </p>
+      <p><b>论坛召集人</b></p>
+      <table style="width: 90%; margin: auto">
+        <tbody>
+          <tr>
+            <td style="width: 75%; padding-right: 20px">
+              <p style="text-align: justify"><b>贾晓红</b>，
+                中国科学院数学与系统科学研究院研究员，中科院数学机械化中心副主任、中国数学会计算机数学专委会副主任委员。
+                主持国家重点研发计划“自由曲线曲面设计与求交理论与方法”青年科学家项目、国家基金委优秀青年基金项目。
+                曾获第五届中国工业与应用数学学会应用数学青年科技奖、第二届全国“几何设计与计算”青年学者奖、入选中科院数学院陈景润未来之星、首届海外优秀青年人才计划等。
+                研究方向为曲面求交、碰撞检测、奇点计算、拓扑判定等</p>
+              邮箱：<span style="color:rgb(52, 152, 221)">xhjia@amss.ac.cn</span>
+            </td>
+            <td style="width: 25%; vertical-align: top">
+              <img src="../../assets/ztlt/jxh.png" style="display: block; margin: 10px; width: 80%"
+                onclick="window.open('http://www.mmrc.iss.ac.cn/~xhjia/')" title="查看主页">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <hr style="width: 90%; margin: auto">
+
+      <p><b>论坛讲者1</b></p>
+      <table style="width: 90%; margin: auto">
+        <tbody>
+          <tr>
+            <td style="width: 75%; padding-right: 20px">
+              <p style="text-align: justify"><b>陈发来</b>，
+                中国科学技术大学数学科学学院教授、博导。曾担任中国工业应用数学学会常务理事（2004-2016），中国工业应用数学学会几何设计与计算专委会主任（2015-1019），中国计算数学学会常务理事（2004—2012），安徽省数学会秘书长（2003-2019），国务院学位委员会数学学科评审组成员（2009-2019），教育部高等学校数学与统
+                计学教学指导委员会委员（2006-2022），第十二、十三届国家自然科学基金委员会数学学科评委，《Computer Aided Geometric Design》,《Visual
+                Computer》，《Numerical Mathematics: Theory, Methods and
+                Applications》，《计算机辅助设计与图形学学报》编委。曾于1997年，2001年两次获国家级教学成果二等奖。2001年获教育部高校青年教师奖，
+                2002年获国家“杰出青年”科学基金资助，2003年获宝钢优秀教师奖特等奖，2008年获中国计算机图形学杰出奖， 2009年获冯康科学计算奖，2010年获全国百篇优博论文指导教师奖，
+                2016年获中组部“万人计划”教学名师。
+                研究方向为计算机辅助几何设计与几何建模。近来感兴趣的研究课题包括：曲面隐式化的动曲面方法，曲面求交，T网格上的样条曲面，等几何分析及其在拓扑优化中的应用，基于稀疏优化的几何处理等。
+              </p>
+              邮箱：<span style="color:rgb(52, 152, 221)">chenfl@ustc.edu.cn</span>
+            </td>
+            <td style="width: 25%; vertical-align: top">
+              <img src="../../assets/ztlt/cfl.jpg" style="display: block; margin: 10px; width: 80%"
+                onclick="window.open('http://staff.ustc.edu.cn/~chenfl/ ')" title="查看主页">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p><b>论坛讲者2</b></p>
+      <table style="width: 90%; margin: auto">
+        <tbody>
+          <tr>
+            <td style="width: 75%; padding-right: 20px">
+              <p style="text-align: justify"><b>李华</b>，中科院计算所研究员，中国科学院大学岗位教授。1982.2毕业于北京航空学院，1986.2
+                在北航获硕士学位，唐荣锡指导；1989.3在中科院计算所获博士学位，刘慎权指导；随后留学德国费劳恩霍菲学会图形与数据处理研究所，Encarnacao指导。1991.4开始在计算所从事CAD、图形图像及可视化研究，1994.10研究员；曾任中科院计算所CAD开放实验室主任，CCF
+                CAD与图形学专委会主任，获得过国家科技进步奖；参与过国产飞机的外形设计；《CAD与图形学学报》副主编；CSIAM 终身会员。
+              </p>
+              邮箱：<span style="color:rgb(52, 152, 221)">chenfl@ustc.edu.cn</span>
+            </td>
+            <td style="width: 25%; vertical-align: top">
+              <img src="../../assets/ztlt/lh.jpg" style="display: block; margin: 10px; width: 80%"
+                onclick="window.open('https://people.ucas.ac.cn/http:/people.ucas.ac.cn/~_lihua')" title="查看主页">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p><b>论坛讲者3</b></p>
+      <table style="width: 90%; margin: auto">
+        <tbody>
+          <tr>
+            <td style="width: 75%; padding-right: 20px">
+              <p style="text-align: justify">
+                <b>刘继红</b>，北京航空航天大学机械工程及自动化学院教授、博士生导师。1996年取得日本东京都立大学机械工程工学博士学位。
+                1996-2004年任职于华中科技大学机械科学与工程学院，曾担任国家CAD支撑软件工程技术研究中心（武汉）副主任
+                主要从事复杂产品数字化设计制造、现代设计理论与方法、知识管理与知识工程、智能制造与工业互联网等方向的研究教学工作。
+                中国机械工程学会机械工业自动化分会副主任委员，《计算机辅助设计与图形学学报》副主编，国家重点研发计划网络协同制造和智能工厂重点专项总体专家组成员。
+              </p>
+              邮箱：<span style="color:rgb(52, 152, 221)">ryukeiko@buaa.edu.cn</span>
+            </td>
+            <td style="width: 25%; vertical-align: top">
+              <img src="../../assets/ztlt/ljh.jpg" style="display: block; margin: 10px; width: 80%">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p><b>论坛讲者4</b></p>
+      <table style="width: 90%; margin: auto">
+        <tbody>
+          <tr>
+            <td style="width: 75%; padding-right: 20px">
+              <p style="text-align: justify">
+                <b>汪国平</b>，现为北京大学计算机系教授。1997年获复旦大学博士学位。长期从事图形学和虚拟现实等方向研究，并担任北京市虚拟仿真与可视化工程中心主任，北大图形与交互技术实验室主任。
+                汪国平教授主持研发了分布式虚拟仿真支撑平台、面向飞行模拟器的视景仿真系统、基于图像的三维重建系统、离散设计与优化系统、物理仿真引擎等多个实用系统，在国防军事、工程设计和文化娱乐等领域有良好应用。曾获教育部科技进步一等奖、2009年获国家杰出青年基金资助。在ACM
+                TOG、IEEE TVCG、CGF等国内外期刊发表学术论文200余篇，国家发明专利50余项。现为CCF CADCG专委会主任、CSIAM
+                GDC专委会主任、中国图象图形学会副理事长、图学学报主编和VR&IH副主编等。
+
+              </p>
+              邮箱：<span style="color:rgb(52, 152, 221)">wgp@graphics.pku.edu.cn</span>
+            </td>
+            <td style="width: 25%; vertical-align: top">
+              <img src="../../assets/ztlt/wgp.jpg" style="display: block; margin: 10px; width: 80%">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+
+      <p><b>论坛讲者5</b></p>
+      <table style="width: 90%; margin: auto">
+        <tbody>
+          <tr>
+            <td style="width: 75%; padding-right: 20px">
+              <p style="text-align: justify">
+                <b>冯结青</b>，，浙江大学CAD&CG国家重点实验室教授，博士生导师。1992年毕业于国防科技大学系统工程与应用数学系，获工学学士学位；1997年毕业于浙江大学应用数学系，获计算几何与图形学博士学位。1997～1999年先后在法国斯特拉斯堡大学计算机科学系和香港中文大学计算机科学与工程学系从事访问研究。1999.2年加入浙江大学CAD&CG国家重点实验室，2003.12晋升研究员，2005.1遴选为博士生导师，2011.1转聘为教授。冯结青一直从事计算机图形学和计算机视觉的基础与应用基础研究，研究工作曾获2009年教育部自然科学奖一等奖、CASA2014和CAD/Graphics2019大会最佳论文奖，指导研究生先后四次获CCF
+                CAD/CG优秀学生论文奖。2010年入选教育部“新世纪优秀人才支持计划”。目前担任《图学学报》副主编、《计算机辅助设计与图形学学报》编委、《Visual Computing for Industry,
+                Biomedicine and
+                Art》编委、《计算机科学》编委、CSIAM第四、五届几何设计与计算专业委员会副主任、CCF第九届计算机辅助设计与图形学专业委员会副主任、中国图象图形学学会第七、八届理事会理事、中国图学会第八届理事会理事等。
+              </p>
+              邮箱：<span style="color:rgb(52, 152, 221)">jqfeng@cad.zju.edu.cn</span>
+            </td>
+            <td style="width: 25%; vertical-align: top">
+              <img src="../../assets/ztlt/fjq.jpg" style="display: block; margin: 10px; width: 80%"
+                onclick="window.open('http://www.cad.zju.edu.cn/home/jqfeng/')" title="查看主页">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p><b>论坛讲者6</b></p>
+      <table style="width: 90%; margin: auto">
+        <tbody>
+          <tr>
+            <td style="width: 75%; padding-right: 20px">
+              <p style="text-align: justify">
+                <b>梅敬成</b>, 法国国立工艺学院机械工程博士，归国前曾就职法国马特拉资讯、达索系统等世界知名CAD软件公司，参与Euclid、CATIA、OpenCasCade等产品和内核的研发。
+                2011年初回国全职加入山东山大华天软件有限公司，担任首席技术官, 带领团队研发具有自主知识产权的国产三维CAD软件SINOVATION、三维模型轻量化浏览器SView。
+                2019年在华天软件的大力支持下，创办山东华云三维科技有限公司，出任总经理，致力于国内首款、完全自主的基于云架构的三位CAD平台CrownCAD的研发和应用推广。
+              </p>
+              邮箱：<span style="color:rgb(52, 152, 221)">jingcheng_mei@163.com</span>
+            </td>
+            <td style="width: 25%; vertical-align: top">
+              <img src="../../assets/ztlt/mjc.jpg" style="display: block; margin: 10px; width: 80%">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p><b>论坛讲者7</b></p>
+      <table style="width: 90%; margin: auto">
+        <tbody>
+          <tr>
+            <td style="width: 75%; padding-right: 20px">
+              <p style="text-align: justify">
+                <b>陈志杨</b>，杭州新迪数字工程系统有限公司CTO，长期从事三维CAD软件研发，具有丰富CAD软件理论和开发实践经验。浙江大学机械工程学院博士、浙江大学计算机科学与技术学院博士后。中国图学会理事、中国图学会企业工作委员会副主任委员、浙江工业软件产业技术联盟副理事长、东南大学机械工程学院硕士生导师（校外）、全国信息技术标准化技术委员会云计算标准工作组（CCWG）技术专家。</p>
+              邮箱：<span style="color:rgb(52, 152, 221)">chen.zhiyang@newdimchina.com</span>
+            </td>
+            <td style="width: 25%; vertical-align: top">
+              <img src="../../assets/ztlt/czy.png" style="display: block; margin: 10px; width: 80%">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+
+
 
 
   </div>
@@ -29,4 +210,19 @@ export default {
   color: #898989;
   font-size: 15px;
 }
+
+.content-main{
+    /* --main-color: #3498db;
+    --minor-color: #1c1f88;
+    --main-width: 1000px;
+    --small-width: 760px;
+    --border-color: #e8e8e8;
+    --header-height: 80px; */
+    color: var(--font-color);
+    margin: 20px 0 0 0;
+    padding: 0 0 8px 0;
+    font-size: 20px;
+    border-bottom: 4px solid #d8d8d8;
+}
+
 </style>
