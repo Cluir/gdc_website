db8973b9c30fb7eb6c251b370411b2346cadebbe
cqk <345978408@qq.com>
2022-08-06 22:23:33 +0800

    update 计算几何40年

--- src/components/heads/js.vue
+++ src/components/heads/js.vue
@@ -6,158 +6,171 @@
     </div>
     <div class="pp" style="text-align:justify;">
       <p>1982年1月，我国著名数学家苏步青先生领导成立全国计算几何协作组，并在7月于青岛成功召开了第一次全国计算几何讨论会。
-      计算几何协作组成立以来，凝聚了一大批来自全国几何设计与计算、计算机图形学等领域的高校、科研院所和企业工作者，
-      开创了我国计算机辅助设计与图形学及其在工业领域的应用，培养了一批几何设计与计算、计算机图形学等领域的卓越人才。自2000年中国工业与应用数学学会几何设计与计算专业委员会成立以来，继承和发扬了计算几何协作组的精神和事业，将我国计算机辅助设计与图形学的发展推到了新的高度。2022年计算几何协作组将迎来成立40周年，同时恰逢苏步青先生诞辰120周年，专委会将在GDC 2022期间举办特别庆祝活动，邀请几何设计与计算领域的老中青科研工作者，回顾协作组以及专委四十年的发展历程，研讨与展望学科发展。
+        计算几何协作组成立以来，凝聚了一大批来自全国几何设计与计算、计算机图形学等领域的高校、科研院所和企业工作者，
+        开创了我国计算机辅助设计与图形学及其在工业领域的应用，培养了一批几何设计与计算、计算机图形学等领域的卓越人才。自2000年中国工业与应用数学学会几何设计与计算专业委员会成立以来，继承和发扬了计算几何协作组的精神和事业，将我国计算机辅助设计与图形学的发展推到了新的高度。2022年计算几何协作组将迎来成立40周年，同时恰逢苏步青先生诞辰120周年，专委会将在GDC
+        2022期间举办特别庆祝活动，邀请几何设计与计算领域的老中青科研工作者，回顾协作组以及专委四十年的发展历程，研讨与展望学科发展。
       </p>
-    <br/>
-    <div class="introduce_2">
-      <h3>计算几何四十年历程</h3>
-      <span>第十四届全国几何设计与计算学术会议</span>
-    </div>
+      <br />
+      <div class="introduce_2">
+        <h3>计算几何四十年历程</h3>
+        <span>第十四届全国几何设计与计算学术会议</span>
+      </div>
       <p>
-全国几何设计与计算学术会议由中国工业与应用数学学会几何设计与计算专业委员会组织，从2002年在青岛召开第一届会议起，每两年举办一次。从2013年起，改为每年举办一次。部分往届会议信息如下:
+        全国几何设计与计算学术会议由中国工业与应用数学学会几何设计与计算专业委员会组织，从2002年在青岛召开第一届会议起，每两年举办一次。从2013年起，改为每年举办一次。部分往届会议信息如下:
 
       </p>
     </div>
     <div class="box">
-        <div class="box_top">
-          <div class="box_1 box_left">
-            <div class="box_dingwei">
-              <h3>2002</h3>
-              <p>青岛市，山东大学承办，青岛大学协办</p>
-              <p>大会名誉主席：苏步青、陈秉聪</p>
-              <p>大会主席：唐荣锡</p>
-              <p>副主席：邵峰晶</p>
-              <p>程序委员会主席：孙家广</p>
-              <p>副主席：汪嘉业、汪国昭、方漪</p>
-            </div>
-          </div>
-          <div class="box_1 box_right">
-            <div class="box_dingwei">
-              <h3>2005</h3>
-              <p>合肥市与黄山市，中国科学技术大学承办</p>
-              <p>大会主席：孙家广</p>
-              <p>副主席：常庚哲、汪国昭</p>
-              <p>程序委员会主席：陈发来</p>
-              <p>副主席：胡事民，王文平、张彩明</p>
-              <p>组织委员会主席：邓建松</p>
-            </div>
+      <div class="box_top">
+        <div class="box_1 box_left">
+          <div class="box_dingwei">
+            <h3>2002</h3>
+            <p>青岛市，山东大学承办，青岛大学协办</p>
+            <p>大会名誉主席：苏步青、陈秉聪</p>
+            <p>大会主席：唐荣锡</p>
+            <p>副主席：邵峰晶</p>
+            <p>程序委员会主席：孙家广</p>
+            <p>副主席：汪嘉业、汪国昭、方漪</p>
           </div>
         </div>
-        <div class="box_top">
-          <div class="box_1 box_left">
-            <div class="box_dingwei">
-              <h3>2007</h3>
-              <p>兰州市和敦煌市，西北师范大学承办</p>
-              <p>大会主席：孙家广</p>
-              <p>副主席：王利明（西北师范大学校长）、汪国昭</p>
-              <p>程序委员会主席：王国瑾</p>
-              <p>副主席：胡事民、陈发来、张彩明、吴宗敏、王文平</p>
-              <p>组织委员会主席：张贵仓</p>
-            </div>
-          </div>
-          <div class="box_1 box_right">
-            <div class="box_dingwei">
-              <h3>2009</h3>
-              <p>厦门，厦门大学承办</p>
-              <p>大会主席：孙家广</p>
-              <p>副主席：胡事民、林亚南</p>
-              <p>程序委员会主席：王文平</p>
-              <p>副主席：邓建松、刘利刚</p>
-              <p>组织委员会主席：曾晓明</p>
-            </div>
+        <div class="box_1 box_right">
+          <div class="box_dingwei">
+            <h3>2005</h3>
+            <p>合肥市与黄山市，中国科学技术大学承办</p>
+            <p>大会主席：孙家广</p>
+            <p>副主席：常庚哲、汪国昭</p>
+            <p>程序委员会主席：陈发来</p>
+            <p>副主席：胡事民，王文平、张彩明</p>
+            <p>组织委员会主席：邓建松</p>
           </div>
         </div>
-        <div class="box_top">
-          <div class="box_1 box_left">
-            <div class="box_dingwei">
-              <h3>2011</h3>
-              <p>广州，华南农业大学承办</p>
-              <p>大会主席：胡事民</p>
-              <p>程序委员会主席：张彩明</p>
-              <p>副主席：刘利刚、李桂清</p>
-              <p>组织委员会主席：朱同林</p>
-              <p><br/></p>
-            </div>
+      </div>
+      <div class="box_top">
+        <div class="box_1 box_left">
+          <div class="box_dingwei">
+            <h3>2007</h3>
+            <p>兰州市和敦煌市，西北师范大学承办</p>
+            <p>大会主席：孙家广</p>
+            <p>副主席：王利明（西北师范大学校长）、汪国昭</p>
+            <p>程序委员会主席：王国瑾</p>
+            <p>副主席：胡事民、陈发来、张彩明、吴宗敏、王文平</p>
+            <p>组织委员会主席：张贵仓</p>
           </div>
-          <div class="box_1 box_right">
-            <div class="box_dingwei">
-              <h3>2013</h3>
-              <p>大连，大连理工大学承办</p>
-              <p>大会主席：王仁宏、张彩明</p>
-              <p>程序委员会主席：罗钟铉</p>
-              <p><br/></p>
-              <p><br/></p>
-              <p><br/></p>
-            </div>
+        </div>
+        <div class="box_1 box_right">
+          <div class="box_dingwei">
+            <h3>2009</h3>
+            <p>厦门，厦门大学承办</p>
+            <p>大会主席：孙家广</p>
+            <p>副主席：胡事民、林亚南</p>
+            <p>程序委员会主席：王文平</p>
+            <p>副主席：邓建松、刘利刚</p>
+            <p>组织委员会主席：曾晓明</p>
+          </div>
+        </div>
+      </div>
+      <div class="box_top">
+        <div class="box_1 box_left">
+          <div class="box_dingwei">
+            <h3>2011</h3>
+            <p>广州，华南农业大学承办</p>
+            <p>大会主席：胡事民</p>
+            <p>程序委员会主席：张彩明</p>
+            <p>副主席：刘利刚、李桂清</p>
+            <p>组织委员会主席：朱同林</p>
+            <p><br /></p>
+          </div>
+        </div>
+        <div class="box_1 box_right">
+          <div class="box_dingwei">
+            <h3>2013</h3>
+            <p>大连，大连理工大学承办</p>
+            <p>大会主席：王仁宏、张彩明</p>
+            <p>程序委员会主席：罗钟铉</p>
+            <p><br /></p>
+            <p><br /></p>
+            <p><br /></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="box_top">
+        <div class="box_1 box_left">
+          <div class="box_dingwei">
+            <h3>2015</h3>
+            <p>杭州，浙江理工大学和浙江工业大学联合承办</p>
+            <p>大会主席：裘松良、张彩明</p>
+            <p>程序委员会主席：马利庄</p>
+            <p>组织委员会主席：李重、寿华好</p>
+            <p><br /></p>
+            <p><br /></p>
+          </div>
+        </div>
+        <div class="box_1 box_right">
+          <div class="box_dingwei">
+            <h3>2016</h3>
+            <p>合肥，中国科学技术大学承办</p>
+            <p>大会主席：陈发来</p>
+            <p>程序委员会主席：汪国平、徐凯、黄章进</p>
+            <p><br /></p>
+            <p><br /></p>
+            <p><br /></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="box_top">
+        <div class="box_1 box_left">
+          <div class="box_dingwei">
+            <h3>2017</h3>
+            <p>山东烟台，山东工商学院承办</p>
+            <p>大会共同主席：徐世艾、陈发来</p>
+            <p>程序委员会主席：冯结青、缪永伟、杨周旺</p>
+            <p><br /></p>
+            <p><br /></p>
+            <p><br /></p>
+          </div>
+        </div>
+        <div class="box_1 box_right">
+          <div class="box_dingwei">
+            <h3>2018</h3>
+            <p>桂林，桂林电子科技大学</p>
+            <p>大会共同主席：陈发来、罗钟铉、罗笑南</p>
+            <p>程序委员会主席：刘永进、章敏</p>
+            <p><br /></p>
+            <p><br /></p>
+            <p><br /></p>
           </div>
         </div>
 
-        <div class="box_top">
-          <div class="box_1 box_left">
-            <div class="box_dingwei">
-              <h3>2015</h3>
-              <p>杭州，浙江理工大学和浙江工业大学联合承办</p>
-              <p>大会主席：裘松良、张彩明</p>
-              <p>程序委员会主席：马利庄</p>
-              <p>组织委员会主席：李重、寿华好</p>
-              <p><br/></p>
-              <p><br/></p>
-            </div>
-          </div>
-          <div class="box_1 box_right">
-            <div class="box_dingwei">
-              <h3>2016</h3>
-              <p>合肥，中国科学技术大学承办</p>
-              <p>大会主席：陈发来</p>
-              <p>程序委员会主席：汪国平、徐凯、黄章进</p>
-              <p><br/></p>
-              <p><br/></p>
-              <p><br/></p>
-            </div>
-          </div>
       </div>
 
-     <div class="box_top">
-          <div class="box_1 box_left">
-            <div class="box_dingwei">
-              <h3>2017</h3>
-              <p>山东烟台，山东工商学院承办</p>
-              <p>大会共同主席：徐世艾、陈发来</p>
-              <p>程序委员会主席：冯结青、缪永伟、杨周旺</p>
-              <p><br/></p>
-              <p><br/></p>
-              <p><br/></p>
-            </div>
+      <div class="box_top">
+        <div class="box_1 box_left">
+          <div class="box_dingwei">
+            <h3>2021</h3>
+            <p>长沙，国防科技大学承办</p>
+            <p>大会共同主席：汪国平、卢凯</p>
+            <p>程序委员会主席：冯结青、刘秀平、张举勇</p>
+            <p><br /></p>
+            <p><br /></p>
+            <p><br /></p>
           </div>
-          <div class="box_1 box_right">
-            <div class="box_dingwei">
-              <h3>2021</h3>
-              <p>长沙，国防科技大学承办</p>
-              <p>大会共同主席：汪国平、卢凯</p>
-              <p>程序委员会主席：冯结青、刘秀平、张举勇</p>
-              <p><br/></p>
-              <p><br/></p>
-              <p><br/></p>
-            </div>
+        </div>
+        <div class="box_1 box_right">
+          <div class="box_dingwei">
+            <h3>2022</h3>
+            <p>山东青岛，山东大学承办</p>
+            <p>大会主席： 李术才、汪嘉业、汪国平</p>
+            <p>程序委员会共同主席： 刘永进、贾晓红、徐岗</p>
+            <p>组织委员会共同主席： 屠长河、汪云海、吕琳、辛士庆</p>
+            <p><br /></p>
+            <p><br /></p>
           </div>
+        </div>
       </div>
 
-        <div class="box_top">
-          <div class="box_1 box_last">
-            <div class="box_dingwei">
-              <h3>2022</h3>
-              <p>山东青岛，山东大学承办</p>
-              <p>大会主席： 李术才、汪嘉业、汪国平</p>
-              <p>程序委员会共同主席： 刘永进、贾晓红、徐岗</p>
-              <p>组织委员会共同主席： 屠长河、汪云海、吕琳、辛士庆</p>
-              <p><br/></p>
-              <p><br/></p>
-            </div>
-          </div>
-      </div>
 
-      
 
 
     </div>
@@ -167,53 +180,59 @@
 
 <script>
 export default {
-    name:'js'
+  name: 'js'
 }
 </script>
 
 <style>
-
 .introduce {
   width: 300px;
   text-align: center;
   margin: 40px auto;
   font-size: 30px;
 }
+
 .introduce_1 {
   width: 1000px;
   text-align: center;
   margin: 40px auto;
   font-size: 30px;
 }
-.introduce_1 > span {
+
+.introduce_1>span {
   display: inline-block;
   margin-top: 10px;
   color: #898989;
   font-size: 15px;
 }
+
 .introduce_2 {
   width: 500px;
   text-align: center;
   margin: 40px auto;
   font-size: 30px;
 }
-.introduce_2 > span {
+
+.introduce_2>span {
   display: inline-block;
   margin-top: 10px;
   color: #898989;
   font-size: 15px;
 }
-.introduce > span {
+
+.introduce>span {
   display: inline-block;
   margin-top: 10px;
   color: #898989;
   font-size: 15px;
 }
+
 .pp {
   text-indent: 40px;
   font-size: 20px;
-  line-height:1.5;
+  line-height: 1.5;
 }
+
 /* 内容文案 */
 .left {
   float: left;
@@ -222,9 +241,11 @@ export default {
   box-sizing: border-box;
   margin-top: -20px;
 }
+
 .right {
   float: left;
 }
+
 .Copywriting_span {
   display: inline-block;
   font-size: 90px;
@@ -232,6 +253,7 @@ export default {
   font-weight: bold;
   text-shadow: -2px 1px 5px rgb(31 103 179);
 }
+
 .Copywriting_P {
   display: inline-block;
   font-size: 82px;
@@ -240,14 +262,17 @@ export default {
   text-shadow: -2px 1px 5px rgb(31 103 179);
   margin-top: -23px;
 }
+
 .right_p {
   text-indent: 2em;
   width: 697px;
   /* letter-spacing:1px */
 }
+
 .right_pp {
   margin-top: 10px;
 }
+
 .bottom_txt {
   text-indent: 2em;
 }
@@ -256,6 +281,7 @@ export default {
 .box {
   margin-top: 10px;
 }
+
 .box_1 {
   width: 460px;
   /* height: 300px; */
@@ -265,25 +291,30 @@ export default {
   /* padding-left: 10px; */
   box-sizing: border-box;
 }
+
 /* .box_top{
     margin-top: 20px;
 	} */
 .box_dingwei {
   position: relative;
 }
+
 .box_left {
   float: left;
   margin-top: 50px;
 }
+
 .box_last {
   margin-top: 50px;
 }
+
 .box_right {
   margin-top: 50px;
   margin-left: 40px;
   float: left;
 }
-.box_dingwei > h3 {
+
+.box_dingwei>h3 {
   position: absolute;
   top: -50px;
   left: 1px;
@@ -292,108 +323,121 @@ export default {
   font-weight: bold;
   text-shadow: -2px 1px 5px rgb(31 103 179);
 }
+
 @media screen and (max-width:500px) {
   .g-rep {
     margin: 0 auto;
-    width:100%;
-}
- .subject .introduce {
-  width: 100%;
-  text-align: center;
-  margin: 3vw auto;
-  font-size: 7vw;
-}
-.introduce > span {
-  display: inline-block;
-  margin-top: 10px;
-  color: #898989;
-  font-size: 15px;
-}
-/* 内容文案 */
-.left {
-  float: none;
-  text-align: center;
-margin: auto;
-  /* width: 258px; */
-  padding: 2vw;
-  box-sizing: border-box;
- display: flex;
-}
-.right {
+    width: 100%;
+  }
 
+  .subject .introduce {
+    width: 100%;
+    text-align: center;
+    margin: 3vw auto;
+    font-size: 7vw;
+  }
 
-}
-.Copywriting{
-  width: 100%;
-}
-/* .Copywriting>p{
+  .introduce>span {
+    display: inline-block;
+    margin-top: 10px;
+    color: #898989;
+    font-size: 15px;
+  }
+
+  /* 内容文案 */
+  .left {
+    float: none;
+    text-align: center;
+    margin: auto;
+    /* width: 258px; */
+    padding: 2vw;
+    box-sizing: border-box;
+    display: flex;
+  }
+
+  .right {}
+
+  .Copywriting {
+    width: 100%;
+  }
+
+  /* .Copywriting>p{
   display: none;
 } */
-.Copywriting_span {
-  display: inline-block;
-  font-size: 13vw;
-  color: white;
-  font-weight: bold;
-  text-shadow: -2px 1px 5px rgb(31 103 179);
-   margin-left: 10vw;
-}
-.Copywriting_P {
-  display: inline-block;
-  font-size: 13vw;
-  color: white;
-  font-weight: bold;
-  text-shadow: -2px 1px 5px rgb(31 103 179);
-  margin-top: 0;
-  margin-left: 10vw;
-}
-.right_p {
-  text-indent: 2em;
-  width: 100%;
-  /* letter-spacing:1px */
-}
-.right_pp {
-  margin-top: 10px;
-}
-.bottom_txt {
-  text-indent: 2em;
-}
+  .Copywriting_span {
+    display: inline-block;
+    font-size: 13vw;
+    color: white;
+    font-weight: bold;
+    text-shadow: -2px 1px 5px rgb(31 103 179);
+    margin-left: 10vw;
+  }
 
-/* <!-- 盒子 --> */
-.box {
-  margin-top: 10px;
-}
-.box_1 {
-  width: 100%;
-  /* height: 300px; */
-  box-shadow: 0 0 8px rgb(124 124 124);
-  display: inline-block;
-  padding:4vw 4vw 4vw 4vw ;
-  /* padding-left: 10px; */
-  box-sizing: border-box;
-}
-/* .box_top{
+  .Copywriting_P {
+    display: inline-block;
+    font-size: 13vw;
+    color: white;
+    font-weight: bold;
+    text-shadow: -2px 1px 5px rgb(31 103 179);
+    margin-top: 0;
+    margin-left: 10vw;
+  }
+
+  .right_p {
+    text-indent: 2em;
+    width: 100%;
+    /* letter-spacing:1px */
+  }
+
+  .right_pp {
+    margin-top: 10px;
+  }
+
+  .bottom_txt {
+    text-indent: 2em;
+  }
+
+  /* <!-- 盒子 --> */
+  .box {
+    margin-top: 10px;
+  }
+
+  .box_1 {
+    width: 100%;
+    /* height: 300px; */
+    box-shadow: 0 0 8px rgb(124 124 124);
+    display: inline-block;
+    padding: 4vw 4vw 4vw 4vw;
+    /* padding-left: 10px; */
+    box-sizing: border-box;
+  }
+
+  /* .box_top{
     margin-top: 20px;
 	} */
-.box_dingwei {
-  position: relative;
-}
-.box_left {
-  float: left;
-  margin-top: 10vw;
-}
-.box_right {
-  margin-top:10vw;
-  margin-left: 0;
-  float: left;
-}
-.box_dingwei > h3 {
-  position: absolute;
-  top: -50px;
-  left: 1px;
-  font-size: 40px;
-  color: white;
-  font-weight: bold;
-  text-shadow: -2px 1px 5px rgb(31 103 179);
-}
+  .box_dingwei {
+    position: relative;
+  }
+
+  .box_left {
+    float: left;
+    margin-top: 10vw;
+  }
+
+  .box_right {
+    margin-top: 10vw;
+    margin-left: 0;
+    float: left;
+  }
+
+  .box_dingwei>h3 {
+    position: absolute;
+    top: -50px;
+    left: 1px;
+    font-size: 40px;
+    color: white;
+    font-weight: bold;
+    text-shadow: -2px 1px 5px rgb(31 103 179);
+  }
 }
 </style>
