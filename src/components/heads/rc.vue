247a1de348101f3c536c4a076a09ea60511007b0
cqk <345978408@qq.com>
2022-08-11 16:24:14 +0800

    change meeting_schedule

--- src/components/heads/rc.vue
+++ src/components/heads/rc.vue
@@ -9,17 +9,25 @@
                 <th colspan="4">2022年8月19日 星期五 全天会议注册</th>
             </tr>
             <tr>
-                <td style="text-align: center; width: 20%"><b>时间</b></td>
-                <td style="text-align: center; width: 50%"><b>日程安排</b></td>
+                <td style="text-align: center; width: 15%"><b>时间</b></td>
+                <td style="text-align: center; width: 40%"><b>报告题目</b></td>
+                <td style="text-align: center; width: 25%"><b>讲者</b></td>
+                <td style="text-align: center; width: 10%"><b>主持人</b></td>
             </tr>
             <tr>
                 <td rowspan="2">14:00—17:00 </td>
-                <td style="text-align: left">
+                <td style="text-align: center">
                     <router-link to="/hq">会前课程1 计图框架入门与进阶</router-link>
+                </td>
+                <td rowspan="2" style="text-align: center">
+                    庞宇飞 郑澎 雷娜 徐岗
+                </td>
+                <td rowspan="2" style="text-align: center">
+                    徐岗
                 </td>
             </tr>
             <tr>
-                <td style="text-align: left">
+                <td style="text-align: center">
                     <router-link to="/hq">会前课程2 网格生成</router-link>
                 </td>
             </tr>
@@ -30,47 +38,51 @@
                 <th colspan="4">2022年8月20日 星期六</th>
             </tr>
             <tr>
-                <td style="text-align: center; width: 20%"><b>时间</b></td>
-                <td style="text-align: center; width: 50%"><b>日程安排</b></td>
+                <td style="text-align: center; width: 15%"><b>时间</b></td>
+                <td style="text-align: center; width: 40%"><b>报告题目</b></td>
+                <td style="text-align: center; width: 25%"><b>讲者</b></td>
             </tr>
             <tr>
                 <td>8:30—9:00</td>
-                <td style="text-align: left">开幕式</td>
+                <td colspan="3" style="text-align: center">开幕式</td>
             </tr>
             <tr>
                 <td>9:00—10:00</td>
-                <td style="text-align: left">
-                    <router-link to="/ty">特邀报告1：神经隐式三维重建与绘制技术 </router-link> &nbsp; &nbsp; <b>报告人:</b>浙江大学 鲍虎军
+                <td style="text-align: center">
+                    <router-link to="/ty">特邀报告1：神经隐式三维重建与绘制技术 </router-link>
+                </td>
+                <td style="text-align: center">
+                    浙江大学 鲍虎军
                 </td>
             </tr>
             <tr>
                 <td>10:20—12:00</td>
-                <td colspan="2" style="text-align: left">全国计算几何协作组四十周年暨苏步青先生诞辰120周年纪念特别论坛</td>
+                <td colspan="2" style="text-align: center">全国计算几何协作组四十周年暨苏步青先生诞辰120周年纪念特别论坛</td>
 
             </tr>
 
             <tr>
                 <td>14:00—16:30</td>
-                <td colspan="2" style="text-align: left">学科发展战略研讨会</td>
+                <td colspan="2" style="text-align: center">学科发展战略研讨会</td>
 
             </tr>
             <tr>
                 <td>16:50—17:20</td>
-                <td colspan="2" style="text-align: left">论文快放1</td>
+                <td colspan="2" style="text-align: center">论文快放1</td>
 
             </tr>
             <tr>
                 <td>17:20—18:00</td>
-                <td colspan="2" style="text-align: left">论文交流1</td>
+                <td colspan="2" style="text-align: center">论文交流1</td>
 
             </tr>
             <tr>
                 <td>18:00—20:00</td>
-                <td colspan="2" style="text-align: left">晚宴</td>
+                <td colspan="2" style="text-align: center">晚宴</td>
             </tr>
             <tr>
                 <td>20:30—21:30</td>
-                <td colspan="2" style="text-align: left">专委会会议</td>
+                <td colspan="2" style="text-align: center">专委会会议</td>
             </tr>
         </table>
         <table class="meeting_schedule">
@@ -78,55 +90,101 @@
                 <th colspan="4">2022年8月21日 星期日</th>
             </tr>
             <tr>
-                <td style="text-align: center; width: 20%"><b>时间</b></td>
-                <td style="text-align: center; width: 50%"><b>日程安排</b></td>
+                <td style="text-align: center; width: 15%"><b>时间</b></td>
+                <td style="text-align: center; width: 40%"><b>报告题目</b></td>
+                <td style="text-align: center; width: 25%"><b>讲者</b></td>
+                <td style="text-align: center; width: 10%"><b>主持人</b></td>
             </tr>
             <tr>
                 <td>8:30—9:30</td>
-                <td style="text-align: left">
-                    <router-link to="/ty">特邀报告2 </router-link> &nbsp; &nbsp; <b>报告人:</b>北京大学 陈宝权
+                <td style="text-align: center">
+                    <router-link to="/ty">特邀报告2 </router-link> &nbsp; &nbsp;
+                </td>
+                <td colspan="2" style="text-align: center">
+                    北京大学 陈宝权
                 </td>
             </tr>
             <tr>
                 <td rowspan="2">9:50—11.30</td>
-                <td style="text-align: left">
+                <td style="text-align: center">
                     <router-link to="/ztlt">专题论坛1 国产CAD几何引擎</router-link>
+                </td>
+                <td style="text-align: center">
+
+                    陈发来 李华
+                    刘继红 汪国平 冯结青 梅敬成 银鸽
+                </td>
+                <td style="text-align: center">
+                    贾晓红
                 </td>
             </tr>
             <tr>
 
-                <td style="text-align: left">
+                <td style="text-align: center">
                     <router-link to="/ztlt">专题论坛2 数字孪生</router-link>
+                </td>
+                <td style="text-align: center">
+                    徐凯 汪俊
+                    刘斌 贾金原
+                    刘红涛
+                </td>
+                <td style="text-align: center">
+                    徐凯
                 </td>
             </tr>
             <tr>
                 <td>11:30—12:00</td>
-                <td style="text-align: left">论文快放2</td>
+                <td colspan="3" style="text-align: center">论文快放2</td>
             </tr>
             <tr>
                 <td>14:00—15:00</td>
-                <td style="text-align: left">
-                    <router-link to="/ty">特邀报告3：数控加工最优路径规划 </router-link> &nbsp; &nbsp; <b>报告人:</b> 中国科学院 高小山
+                <td style="text-align: center">
+                    <router-link to="/ty">特邀报告3：数控加工最优路径规划 </router-link>
+                </td>
+                <td colspan="2" style="text-align: center">
+                    中国科学院 高小山
                 </td>
             </tr>
             <tr>
                 <td>15:00—15:40</td>
-                <td colspan="2" style="text-align: left">论文交流2</td>
+                <td colspan="3" style="text-align: center">论文交流2</td>
             </tr>
             <tr>
                 <td rowspan="2">15:40—17:20</td>
-                <td colspan="2" style="text-align: left">
+                <td style="text-align: center">
                     <router-link to="/ztlt">专题论坛3 开源工业软件</router-link>
+                </td>
+                <td style="text-align: center">
+                    郭旭
+                    何均宏
+                    张文丰 崔涛 王伟
+                    唐滨
+                </td>
+                <td style="text-align: center">
+                    徐岗
                 </td>
             </tr>
 
 
             <tr>
-                <td style="text-align: left">
+                <td style="text-align: center">
                     <router-link to="/ztlt">专题论坛4 深度几何学习</router-link>
                 </td>
-            </tr>
+                <td style="text-align: center">
 
+                    李昌健
+                    李曼祎
+                    李贤芝
+                    张举勇
+                </td>
+                <td style="text-align: center">
+                    胡瑞珍
+                </td>
+            </tr>
+            <tr>
+                <td>19:30—21:00</td>
+                <td colspan="3" style="text-align: center">GDC青年学者之夜</td>
+            </tr>
 
         </table>
         <table class="meeting_schedule">
@@ -134,23 +192,33 @@
                 <th colspan="4">2022年8月22日 星期一</th>
             </tr>
             <tr>
-                <td style="text-align: center; width: 20%"><b>时间</b></td>
-                <td style="text-align: center; width: 50%"><b>日程安排</b></td>
+                <td style="text-align: center; width: 15%"><b>时间</b></td>
+                <td style="text-align: center; width: 40%"><b>报告题目</b></td>
+                <td style="text-align: center; width: 25%"><b>讲者</b></td>
+                <td style="text-align: center; width: 10%"><b>主持人</b></td>
             </tr>
             <tr>
                 <td>8:30—10:00</td>
-                <td style="text-align: left">产业特别论坛 </td>
+                <td colspan="3" style="text-align: center">产业特别论坛 </td>
             </tr>
             <tr>
                 <td>10:10—11:50</td>
-                <td style="text-align: left">
+                <td style="text-align: center">
                     <router-link to="/ztlt">专题论坛5 增材制造</router-link>
+                </td>
+                <td style="text-align: center">
+
+                    李好 刘继凯 王伟明 熊异 邹强
+                </td>
+                <td style="text-align: center">
+                    吕琳
                 </td>
             </tr>
 
+
             <tr>
                 <td>11:50—12:00</td>
-                <td colspan="2" style="text-align: left">闭幕式</td>
+                <td colspan="3" style="text-align: center">闭幕式</td>
             </tr>
         </table>
 
@@ -201,7 +269,7 @@ export default {
 .meeting_schedule th {
     padding-top: 12px;
     padding-bottom: 12px;
-    text-align: left;
+    text-align: center;
     background-color: #2E4D8E;
     color: white;
     text-align: center;
